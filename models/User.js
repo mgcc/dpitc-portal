9bab67638f4023fd9f8a62d082071f897a53425b
JustinAguinaldo <jstnagnldo@up.edu.ph>
2017-06-15 10:49:59 +0800

    Added signup page

--- models/User.js
+++ models/User.js
@@ -20,6 +20,17 @@ User.add({
     many: true
   },
 	location: { type: Types.Location, defaults: { country: 'Philippines' }},
+	agencyAffiliation: {
+		type: Types.Select,
+		options: [
+		{ value: 'Researcher', label: 'Researcher' },
+		{ value: 'Business/Private Sector', label: 'Business/Private Sector' },
+		{ value: 'Policy Maker', label: 'Policy Maker' },
+		{ value: 'Other', label: 'Other' }
+		],
+		initial: false,
+		required: true
+	},
 	birthday: { type: Types.Date, initial: true, required: true, index: true },
 	sex: {
     type: Types.Select,
@@ -30,6 +41,7 @@ User.add({
     initial: false,
     required: false
   },
+	contactNumber: { type: Number, initial: true, required: true, index: true },
 }, 'Permissions', {
 	isAdmin: { type: Boolean, label: 'Can access Keystone', index: false },
   isElearningAdmin: { type: Boolean, label: 'Can access Elearning Admin', index: false},
@@ -53,6 +65,66 @@ User.schema.virtual('canAccessElearningUI').get(function () {
   return this.isElearningUser;
 });
 
+
+// Provide access to Posts
+User.schema.virtual('canAccessPosts').get(function () {
+	return this.isPostsAdmin;
+});
+
+// Provide access to Contents
+User.schema.virtual('canAccessContents').get(function () {
+	return this.isContentsAdmin;
+});
+
+// Provide access to Pages
+User.schema.virtual('canAccessPages').get(function () {
+	return this.isPagesAdmin;
+});
+
+// Provide access to Users
+User.schema.virtual('canAccessUsers').get(function () {
+	return this.isUsersAdmin;
+});
+
+// Provide access to Analytics
+User.schema.virtual('canAccessAnalytics').get(function () {
+	return this.isAnalyticsAdmin;
+});
+
+// Provide access to Community
+User.schema.virtual('canAccessCommunity').get(function () {
+	return this.isCommunityAdmin;
+});
+
+// Provide access to Publications Component
+User.schema.virtual('canAccessPublicationsAdmin').get(function () {
+	return this.isPublicationsAdmin;
+});
+
+User.schema.virtual('canAccessPublicationsUI').get(function () {
+	return this.isPublicationsUser;
+});
+
+// Provide access to Categories
+User.schema.virtual('canAccessCategories').get(function () {
+	return this.isCategoriesAdmin;
+});
+
+//Store user info from sign up page
+User.schema.post('/signup', function(req, res){
+		new user({
+						fname : req.body.first,
+						lname : req.body.lname,
+						password : req.body.password,
+						birthday : req.body.birthday,
+						consumerType : req.body.consumerType,
+						affiliation: req.body.agencyAffiliation,
+						sex : req.body.sex,
+		}) .save(function (err, doc) {
+						if(err) res.json(err);
+						else 		res.send('Succesfully inserted');
+		});
+});
 
 
 /**
